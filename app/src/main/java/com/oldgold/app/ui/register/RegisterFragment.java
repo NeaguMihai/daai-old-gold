af1ccdc76760dfc9ff766195c648a365005571e7
Neagu Mihai <myhay199961@gmail.com>
2021-01-24 22:53:00 +0200

    Working Register function with firebase implementation

--- app/src/main/java/com/oldgold/app/ui/register/RegisterFragment.java
+++ app/src/main/java/com/oldgold/app/ui/register/RegisterFragment.java
@@ -1,36 +1,104 @@
 package com.oldgold.app.ui.register;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.oldgold.app.R;
-import com.oldgold.app.ui.login.LoginViewModel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RegisterFragment extends Fragment {
 
     private RegisterViewModel registerViewModel;
+    private EditText email, password;
+    private TextView name, lastName;
+    private Button btnSignUp;
+    private FirebaseAuth firebaseAuth;
+    private TextView textView;
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle saveInstanceState) {
         registerViewModel = new ViewModelProvider(this).get(RegisterViewModel.class);
         View root = inflater.inflate(R.layout.fragment_register, container, false);
-
-        final TextView textView = root.findViewById(R.id.register_message);
+        textView = root.findViewById(R.id.register_header);
         registerViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 textView.setText(s);
             }
         });
+        setUp(root);
         return root;
 
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void setUp(View root) {
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        name = root.findViewById(R.id.name);
+        lastName = root.findViewById(R.id.last_name);
+        email = root.findViewById(R.id.email);
+        password = root.findViewById(R.id.password);
+        btnSignUp = root.findViewById(R.id.register_button);
+        btnSignUp.setOnClickListener(v -> {
+            root.findViewById(R.id.register_progress_bar).setVisibility(View.VISIBLE);
+            Map<TextView, String> datas = new HashMap<>();
+            datas.put(name, name.getText().toString());
+            datas.put(lastName , lastName.getText().toString());
+            datas.put(email, email.getText().toString());
+            datas.put(password, password.getText().toString());
+            AtomicReference<Boolean> errors = new AtomicReference<>(false);
+            datas.forEach((key, val) -> {
+                if(val.isEmpty()) {
+                    key.setError("field is mandatory");
+                    key.requestFocus();
+                    errors.set(true);
+                }
+            });
+            if (errors.get()) {
+                Toast.makeText(getActivity(), "Fields must not be empty", Toast.LENGTH_SHORT).show();
+                root.findViewById(R.id.register_progress_bar).setVisibility(View.GONE);
+            }
+            else {
+                root.findViewById(R.id.register_progress_bar).setVisibility(View.VISIBLE);
+                firebaseAuth.createUserWithEmailAndPassword(datas.get(email), datas.get(password)).addOnCompleteListener( new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Navigation.findNavController(root).navigate(R.id.nav_login);
+                        }else {
+                            root.findViewById(R.id.register_progress_bar).setVisibility(View.GONE);
+                            Toast.makeText(getActivity(), "Fields must not be empty", Toast.LENGTH_SHORT).show();
+
+
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
 }
--- app/src/main/java/com/oldgold/app/ui/register/RegisterViewModel.java
+++ app/src/main/java/com/oldgold/app/ui/register/RegisterViewModel.java
@@ -1,19 +1,28 @@
 package com.oldgold.app.ui.register;
 
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.List;
 
 public class RegisterViewModel extends ViewModel {
 
     private MutableLiveData<String> data;
 
+
     public RegisterViewModel() {
         data = new MutableLiveData<>();
-        data.setValue("Login view");
+        data.setValue("Register");
     }
 
     public LiveData<String> getText() {
         return  data;
     }
+
 }
